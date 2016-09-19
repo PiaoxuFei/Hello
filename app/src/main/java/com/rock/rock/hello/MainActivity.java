27cc3768cba39d01fca3f136f9c6364c41e9e41d
RockRong1204 <RockRong1204@gmail.com>
2016-09-19 20:48:42 +0800

    测试提交

--- app/src/main/java/com/rock/rock/hello/MainActivity.java
+++ app/src/main/java/com/rock/rock/hello/MainActivity.java
@@ -3,6 +3,9 @@ package com.rock.rock.hello;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 测试修改
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
